f59fcd95c7cbd814db9377733988bae63c073e8e
Sexosexosexo <sonkaos999@gmail.com>
2023-09-12 21:51:45 +0200

    Curve operations

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn encrypt_ecdsa_input(secret_key: &[u8; 32], ck: &ClientKey) -> (Vec<bool>, Vec
     ).serialize();
 
     // Nonce modular inverse
-    let nonce_inverse = bytes_to_bools(&modular_inverse(&nonce));
+    let nonce_inverse = bytes_to_bools(&modular_inverse_n(&nonce));
     let priv_k = bytes_to_bools(secret_key);
 
     let d = encrypt_bools(&priv_k, ck);
@@ -78,12 +78,47 @@ fn encrypt_ecdsa_input(secret_key: &[u8; 32], ck: &ClientKey) -> (Vec<bool>, Vec
     (r, k, d)
 }
 
-fn modular_inverse(base: &[u8; 32]) -> [u8; 32] {
-    let base = U256::from_big_endian(base);
-    let p = U256::from_dec_str("115792089237316195423570985008687907852837564279074904382605163141518161494337").unwrap();
+fn point_addition(point_1: (U512, U512), point_2: (U512, U512)) -> (U512, U512) {
+    let p = U256::from_dec_str("115792089237316195423570985008687907853269984665640564039457584007908834671663").unwrap();
+    let (x1, y1) = point_1;
+    let (x2, y2) = point_2;
+
+    // We add p to avoid underflow
+    let dividend = ((y2 + U512::from(p)) - y1) % p;
+    let divisor = ((x2 + U512::from(p)) - x1) % p;
+
+    let divisor_inverse = modular_inverse_p(divisor);
+
+    let slope = (dividend * divisor_inverse) % p;
+
+    let x3 = ((slope * slope) - x1 - x2) % p;
+    let y3 = ((slope * (((x1 + U512::from(p)) - x3) % p)) - y1) % p;
+
+    (x3, y3)
+}
+
+fn point_doubling(point: (U512, U512)) -> (U512, U512) {
+    let p = U256::from_dec_str("115792089237316195423570985008687907853269984665640564039457584007908834671663").unwrap();
+    let (x, y) = point;
+
+    let dividend = (U512::from(3) * ((x * x) % p)) % p;
+    let divisor = (U512::from(2) * y) % p;
+
+    let divisor_inverse = modular_inverse_p(divisor);
+
+    let slope = (dividend * divisor_inverse) % p;
+
+    let x3 = ((slope * slope) - x - x) % p;
+    let y3 = ((slope * (((x + U512::from(p)) - x3) % p)) - y) % p;
+
+    (x3, y3)
+}
+
+fn modular_inverse_p(base: U512) -> U512 {
+    let p = U256::from_dec_str("115792089237316195423570985008687907853269984665640564039457584007908834671663").unwrap();
 
     let mut res = U512::one();
-    let mut x = U512::from(base % p);
+    let mut x = base;
     let mut y = p - U256::from(2);
 
     while y > U256::zero() {
@@ -92,6 +127,25 @@ fn modular_inverse(base: &[u8; 32]) -> [u8; 32] {
         }
         y /= U256::from(2);
         x = (x * x) % p;
+    }
+
+    res
+}
+
+fn modular_inverse_n(base: &[u8; 32]) -> [u8; 32] {
+    let base = U256::from_big_endian(base);
+    let n = U256::from_dec_str("115792089237316195423570985008687907852837564279074904382605163141518161494337").unwrap();
+
+    let mut res = U512::one();
+    let mut x = U512::from(base % n);
+    let mut y = n - U256::from(2);
+
+    while y > U256::zero() {
+        if y % U256::from(2) != U256::zero() {
+            res = (res * x) % n;
+        }
+        y /= U256::from(2);
+        x = (x * x) % n;
     }
 
     let mut bytes = [0u8; 64];
